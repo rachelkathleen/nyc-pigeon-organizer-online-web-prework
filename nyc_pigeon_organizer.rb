bdd45035f6028a33d8973ce94c6303f02fb9cc10
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-17 16:23:26 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -25,8 +25,8 @@ def nyc_pigeon_organizer(data)
     value.each do |key2, names|
       names.each do |name|
       #binding.pry
-      pigeon_list[name] || = {}
-      pigeon_list[name][key] || = []
+      pigeon_list[name] ||= {}
+      pigeon_list[name][key] ||= []
       pigeon_list[name][key] << key.to_s
       end
     end
