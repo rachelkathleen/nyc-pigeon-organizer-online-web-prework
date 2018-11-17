6a9ac2fc8a773ba332855086e5d3b5a51efed8d3
Rachel Hawa <rachel.hawa@gmail.com>
2018-11-17 16:26:35 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,34 @@
+#pigeon_data = {
+#  :color => {
+#    :purple => ["Theo", "Peter Jr.", "Lucky"],
+#    :grey => ["Theo", "Peter Jr.", "Ms. K"],
+#    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+#    :brown => ["Queenie", "Alex"]
+#  },
+#  :gender => {
+#    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+#    :female => ["Queenie", "Ms. K"]
+#  },
+#  :lives => {
+#    "Subway" => ["Theo", "Queenie"],
+#    "Central Park" => ["Alex", "Ms. K", "Lucky"],
+#    "Library" => ["Peter Jr."],
+#    "City Hall" => ["Andrew"]
+#  }
+#}
+
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+   organized = {}
+   data.each do |trait, options|
+   options.each do |option, pigeons|
+   pigeons.each do |pigeon|
+       organized[pigeon] ||= {}
+       organized[pigeon][trait] ||= []
+       organized[pigeon][trait] << option.to_s
+       end
+     end
+   end
+   organized
 end
